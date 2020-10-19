c9ff5fb0f417a22cd15382e4b5e248120c61375c
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-10-19 11:52:34 +0900

    Create REadingCSVFiles.java

---
+++ REadingCSVFiles.java
@@ -0,0 +1,25 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.File;
+
+
+class Main {
+  public static void main(String[] args) {
+    File dataFile = new File("Countries.csv");
+    try{
+      Scanner input = new Scanner(dataFile);
+      input.useDelimiter(",|\\s");
+      String column1 = input.next();
+      String column2 = input.next();
+      System.out.printf("%-10s%12s%n",column1,column2);
+      while (input.hasNext()){
+        String country = input.next();
+        int population = input.nextInt();
+        System.out.printf("%-10s%,12d%n", country, population);
+      }
+    }catch (FileNotFoundException e) {
+    System.out.println(e);
+    }
+  }
+}
